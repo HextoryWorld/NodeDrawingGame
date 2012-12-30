57d76f65f2c8d078a7e51064a8136bd3b363debe
Jose Manuel Perez Sevilla <jmperez@gmail.com>
2012-12-30 09:36:30 +0100

    gitignore

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ var app = require('http').createServer(handler),
 var fileServer = new nstatic.Server('./');
 	
 // This is the port for our web server.
-// you will need to go to http://localhost:8080 to see it
-app.listen(3000);
+// you will need to go to http://localhost:3000 to see it
+var port = process.env.PORT || 3000; // Cloud9 + Heroku || localhost
+app.listen(port);
 
 // If the URL of the socket server is opened in a browser
 function handler (request, response) {
@@ -24,6 +25,17 @@ function handler (request, response) {
 
 // Delete this row if you want to see debug messages
 io.set('log level', 1);
+
+// Heroku doesn't support websockets so...
+// i detect if i am  on heroku via config vars
+// https://devcenter.heroku.com/articles/config-vars
+// heroku config:add HEROKU=true --app node-drawing-game
+if (process.env.HEROKU === 'true') {
+    io.configure(function () {
+        io.set("transports", ["xhr-polling"]);
+        io.set("polling duration", 10);
+    });
+}
 
 // Listen for incoming connections from clients
 io.sockets.on('connection', function (socket) {
